ef8c1ca04f03adfe1082d2d2f9071af14a654954
MwitaJ <chachajoseph109@gmail.com>
2023-03-13 16:14:51 +0300

    Completed 1st version of this application

---
+++ Controllers/UserController.cs
@@ -0,0 +1,137 @@
+﻿using BookingApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection.Metadata;
+using System.Security.Claims;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+        public UserController(IConfiguration config) 
+        {
+            _config = config;
+           
+        }
+
+
+        // GET Get a user by ID
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Hashtable values = new Hashtable();
+            
+            values["Success"] = true;
+            values["user"] = new MUser()
+            {
+                UserName = User.FindFirst(ClaimTypes.GivenName)?.Value?? "",
+                FullName = User.FindFirst(ClaimTypes.Name)?.Value?? "",
+                UserID = int.Parse(User.FindFirst(ClaimTypes.PrimarySid)?.Value ?? "0"),
+                Email = User.FindFirst(ClaimTypes.Email)?.Value ?? ""
+
+            };
+
+            return new OkObjectResult(values);
+        }
+
+        //Update User
+       [HttpPut]
+        public async Task<IActionResult> Put([FromBody] MUser user)
+        {
+            Hashtable values = new Hashtable(); //Return values in form of a message and result
+
+            //Get user id
+            int userID = int.Parse(User.FindFirst(ClaimTypes.PrimarySid)?.Value ?? "0");
+
+            if (ModelState.IsValid)//Update user
+            {
+                bool response = await CreateUpdateUser(user, userID);
+
+                if (response)
+                {
+                    user.Password = " ";
+                    values.Add("user", user);
+                    values.Add("success", true);
+
+                    return new OkObjectResult(values);
+                }
+
+            }
+
+            //Return values in case of an errror
+            values.Add("Message", "Error occurred while processing your request. Try again");
+            values.Add("Success", false);
+            return new BadRequestObjectResult(values);
+            
+
+           
+        }
+
+        private async Task<bool> CreateUpdateUser(MUser user, int id)
+        {
+            //Catch error while registering
+            try
+            {
+                using (SqlConnection _connection = new SqlConnection(_config.GetConnectionString("connString")))
+                {
+                    _connection.OpenAsync().Wait();
+
+                    using (SqlCommand command = new SqlCommand("spInsertUpdateUser", _connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("userID", SqlDbType.NVarChar).Value = id;
+                        command.Parameters.AddWithValue("userName", SqlDbType.NVarChar).Value = user.UserName;
+                        command.Parameters.AddWithValue("password", SqlDbType.NVarChar).Value = user.Password;
+                        command.Parameters.AddWithValue("email", SqlDbType.NVarChar).Value = user.Email;
+                        command.Parameters.AddWithValue("fullName", SqlDbType.NVarChar).Value = user.FullName;
+                        command.Parameters.AddWithValue("physicalAddress", SqlDbType.NVarChar).Value = user.PhysicalAddress;
+                        command.Parameters.AddWithValue("telephone", SqlDbType.NVarChar).Value = user.Telephone;
+                        command.Parameters.AddWithValue("originCountry", SqlDbType.NVarChar).Value = user.OriginCountry;
+                        command.Parameters.AddWithValue("employerName", SqlDbType.NVarChar).Value = user.EmployerName;
+                        command.Parameters.AddWithValue("experience", SqlDbType.Int).Value = user.Experience;
+                        command.Parameters.AddWithValue("position", SqlDbType.NVarChar).Value = user.Position;
+                        command.Parameters.AddWithValue("disabilityStatus", SqlDbType.NVarChar).Value = user.DisabilityStatus;
+
+                        SqlDataReader reader = await command.ExecuteReaderAsync();
+                        reader.ReadAsync().Wait();
+
+                        if (reader.GetInt32(0) == 1) //Success
+                        
+
+                            return true;
+                       else
+                       
+                            return false;
+
+                    }
+
+                }
+
+
+
+            }
+            catch
+            {
+                //Log error message
+
+                //values.Add("Message", e.Message);
+                //values.Add("Success", false);
+                //return new BadRequestObjectResult(values);
+                return false;
+            }
+
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+using BookingApi;
+using BookingApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+////Add microsoft authenticatio
+//builder.Services.AddAuthentication()
+//    .AddMicrosoftIdentityWebApi(builder.Configuration);
+
+//builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
+
+// Add services to the container.
+
+//Add Jwt authentication and validation middleware
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(ops =>
+{
+    ops.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+    };
+
+    //ops.ForwardSignIn = "/api/Account/Login";
+
+});
+
+//Add authorization here. Can be added using a middleware function
+builder.Services.AddAuthorization(ops =>
+{
+    ops.AddPolicy("AdminUser", policy => policy.RequireClaim(MUser.ADMIN_TYPE, "admin"));
+});
+
+
+builder.Services.AddControllers();
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.UseCors(ops =>
+    {
+        ops.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+}
+
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+//Redirect back if user is not authenticated
+//This middleware ensures that, a user is authenticated before atleast going further into the system
+app.Use(async (Context, next) =>
+{
+    if (!Context.User.Identity.IsAuthenticated)
+    {
+        Context.Response.StatusCode = 401;
+        await Context.Response.WriteAsync("Unauthorized. Try logging in");
+    }
+    else
+    {
+        await next();
+
+    }
+
+});
+
+
+app.MapControllers();
+
+app.Run();
